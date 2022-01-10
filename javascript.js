21e829466f3bf01142d2436ae5c5bf203f750d56
chaihuibin <77320373+chaihuibin926@users.noreply.github.com>
2022-01-10 16:41:04 +0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -43,3 +43,61 @@ function getmaxsubsum(arr) {
 }
 
 alert( getmaxsubsum([1,3,5,1,-3,-2,3]) )
+
+// ax + by = c
+// dx + ey = f
+
+// var a = 4
+// var b = 5
+// var c = 6
+// var d = 7
+// var e = 8
+// var f = 9
+
+// d * (c - b * y) + e * y = f
+
+//合并排列
+function mergeSort(ary) {
+  if (ary.length < 2) return ary
+  let mid = ary.length >> 1
+  let left = ary.slice(0, mid)
+  let right = ary.slice(mid)
+  mergeSort(left)
+  mergeSort(right)
+  let i = 0
+  let j = 0
+  let k = 0
+  while(i < left.length && j < right.length) {
+    if (left[i] >= right[j]) {
+      ary[k++] = right[j++]
+    } else {
+      ary[k++] = left[j++]
+    }
+  }
+  while (i < left.length) {
+    ary[k++] = left[i++]
+  }
+  while (i < right.length) {
+    ary[k++] = right[i++]
+  }
+  return ary
+}
+
+
+//数组转链表
+function ListNode(val, next) {
+  this.val === undefined ? 0 : val
+  this.next === undefined ? null : next
+}
+
+function arrToList(array) {
+  if (array == []) return null
+  let node = new ListNode()
+  let temp = node
+  for (let i = 0; i < array.length; i++) {
+    let p = new ListNode(array[i])
+    temp.next = p
+    temp = p
+  }
+  return node.next
+}
